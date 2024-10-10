2848102329bf7a9b731eb6352affd30da5778131
Lucas Cirillo Berardi <lucascirilloberardi@HP-ProBook-440-G8-Notebook-PC-c0332be8.localdomain>
2024-10-10 10:15:09 +0000

    feat(main.js): great lucas with a function welcome with differents scopes variales

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const globalVar = 'Welcome'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+function welcome(){
+  const funcionVar = "Lucas"
+  console.log('Interviewing Lucas...')
+  let happy = `We are happy to be able to work with you `
+  setTimeout(()=>{
+    happy += funcionVar
+    const blockVar = `${globalVar} to GM2`
+    console.log(blockVar) // En el bloque funciona
+    console.log(happy)
+  }, 3000)
+  //console.log(blockVar) Fuera del bloque devuelve que no esta definida porque es una variable de bloque y solo esta definida dentro del setTimeout, pero happy esta declarada en la funcion, por lo que cambiara y sera devuelta correctamente en el log.
+}
+
+welcome()
