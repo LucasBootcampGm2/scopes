d0a061cbfb129f52a7218c54e19db60463e735c5
Lucas Cirillo Berardi <lucascirilloberardi@HP-ProBook-440-G8-Notebook-PC-c0332be8.localdomain>
2024-10-10 10:38:37 +0000

    feat(exercise2.js): add a log to show how closure works

--- exercise2.js
+++ exercise2.js
@@ -1,4 +1,4 @@
-let globalVar2 = 10;
+let globalVar = 10;
 
 function miFuncion(paramVar) {
   let functionVar = 20;
@@ -7,19 +7,20 @@ function miFuncion(paramVar) {
     let blockVar = 30;
 
     function closureFunc() {
-      globalVar2 += 1;
+      globalVar += 1;
       functionVar += 1;
       blockVar += 1;
 
-      console.log("globalVar:", globalVar2);
+      console.log("globalVar:", globalVar);
       console.log("functionVar:", functionVar);
       console.log("blockVar:", blockVar);
     }
+    console.log("globalVar:", globalVar);
 
     closureFunc();
   }
 }
 
 miFuncion(5);
-
+closureFunc() // Como esta definida en la funcion, su scope es de funcion y no existe afuera
 console.log("Final globalVar value:", globalVar);
