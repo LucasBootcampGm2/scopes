bedcc5de2bf0b81266b5b72a496746b573238475
Lucas Cirillo Berardi <lucascirilloberardi@HP-ProBook-440-G8-Notebook-PC-c0332be8.localdomain>
2024-10-10 11:37:41 +0000

    feat(exercise1.js): change second coment

--- exercise1.js
+++ exercise1.js
@@ -9,7 +9,7 @@ function welcome() {
     console.log(blockVar); // En el bloque funciona
     console.log(funcionVar);
   }, 3000);
-  //console.log(blockVar) Fuera del bloque devuelve que no esta definida porque es una variable de bloque y solo esta definida dentro del setTimeout, pero happy esta declarada en la funcion, por lo que cambiara y sera devuelta correctamente en el log.
+  //console.log(blockVar) Fuera del bloque devuelve que no esta definida porque es una variable de bloque y solo esta definida dentro del setTimeout, pero functionVar esta declarada en la funcion, por lo que cambiara y sera devuelta correctamente en el log.
 }
 
 welcome();
